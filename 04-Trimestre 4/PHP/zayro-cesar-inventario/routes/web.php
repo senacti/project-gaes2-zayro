ba2f700db47ce4195b8b88ba0d76acab135fde34
CesarSanchez02 <cesar.ssanchez02@gmail.com>
2023-05-29 22:02:14 -0500

    inventario-laravel

--- 04-Trimestre 4/PHP/zayro-cesar-inventario/routes/web.php
+++ 04-Trimestre 4/PHP/zayro-cesar-inventario/routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/zayro', function () {
-    return view('zayro');
+Route::get('/inventario', function () {
+    return view('inventario');
 });
